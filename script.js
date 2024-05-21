bb2a4c1610fd70648b3a190fc0e503465bc5a7c7
Siddique-Ahmed <itssiddique786@gmail.com>
2024-05-22 03:16:37 +0500

    light style

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
 let car = document.querySelector("#car");
-let redLight = document.querySelector("#red");
-let yellowLight = document.querySelector("#yellow");
 let greenLight = document.querySelector("#green");
+let yellowLight = document.querySelector("#yellow");
+let redLight = document.querySelector("#red");
 
-function setRedLight() {
-  redLight.style.backgroundColor = "red";
-  greenLight.style.backgroundColor = "green"; 
+
+function setgreenLight() {
+  greenLight.style.backgroundColor = "rgb(5, 235, 5)";
   yellowLight.style.backgroundColor = "rgb(180, 180, 7)";
-  car.style.animationPlayState = "paused"; 
-}
-
-function setYellowLight() {
-  redLight.style.backgroundColor = "rgb(168, 14, 14)"; 
-  greenLight.style.backgroundColor = "green";
-  yellowLight.style.backgroundColor = "yellow";
-  car.style.animationPlayState = "paused"; 
-}
-
-function setGreenLight() {
   redLight.style.backgroundColor = "rgb(168, 14, 14)";
-  greenLight.style.backgroundColor = "rgb(5, 235, 5)"; 
-  yellowLight.style.backgroundColor = "rgb(180, 180, 7)"; 
   car.style.animationPlayState = "running";
 }
-
-function trafficLightSequence() {
-  setGreenLight(); 
-  setTimeout(() => {
-    setYellowLight();
-    setTimeout(() => {
-      setRedLight();
-      setTimeout(() => {
-        trafficLightSequence(); 
-      }, 5000);
-    }, 2000); 
-  }, 5000); 
+function setyellowLight() {
+  greenLight.style.backgroundColor = "green";
+  yellowLight.style.backgroundColor = "yellow";
+  redLight.style.backgroundColor = "rgb(168, 14, 14)";
+  car.style.animationPlayState = "paused";
+}
+function setredLight() {
+  greenLight.style.backgroundColor = "green";
+  yellowLight.style.backgroundColor = "rgb(180, 180, 7)";
+  redLight.style.backgroundColor = "red";
+  car.style.animationPlayState = "paused";
 }
 
 
-trafficLightSequence();
+function lightSequence() {
+  setgreenLight();
+  setTimeout(() => {
+    setyellowLight();
+    setTimeout(() => {
+      setredLight();
+      setTimeout(() => {
+        setyellowLight();
+        setTimeout(() => {
+          setgreenLight();
+          setTimeout(() => {
+            lightSequence()
+          }, 5000);
+        }, 2000);
+      }, 5000);
+    }, 2000);
+  }, 5000);
+}
+
+lightSequence()
